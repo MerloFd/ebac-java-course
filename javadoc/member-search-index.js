3651d1ea4f5a97770be3024521a0bde20e294426
Matheus Merlo <matheus.merlo02@gmail.com>
2025-11-30 21:09:14 -0300

    Squashed 'average-calc/' content from commit 307c221
    git-subtree-dir: average-calc
    git-subtree-split: 307c221910470de489f3561bda7dc0e7060f1637

---
+++ javadoc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"CalcDeMedias","l":"CalcDeMedias()","url":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"CalcDeMedias","l":"main(String[])","url":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"CalcDeMedias","l":"mediaFinal()"},{"p":"<Unnamed>","c":"CalcDeMedias","l":"notasAlunos()"}]
